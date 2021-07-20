4ecaa61f1332688eeba44816aed075c36436a9f9
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-20 22:47:33 +0530

    created server using express

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'server running perfectly'
+    })
+})
+
+app.listen(port, () => {
+    console.log('server is running at port', port);
+})
