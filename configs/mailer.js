c52f113ed469359c59276cbf26ddd2c0e5f8ab65
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-21 22:12:40 +0530

    added mail configuration

---
+++ configs/mailer.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+const fs = require('fs');
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "verify.connectx@gmail.com",
+        pass: process.env.VERIFY_PASSWORD,
+    },
+});
+
+const supporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "help.connectx@gmail.com",
+        pass: process.env.HELP_PASSWORD,
+    },
+});
+
+const readHTMLFile = function (path, callback) {
+    fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
+        callback(null, html);
+    });
+};
+
+module.exports = { transporter, supporter, readHTMLFile };
