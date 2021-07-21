aefa14378207b42489ba9c62d6ad501a14b51580
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-21 13:00:18 +0530

    created model folder for db schema

---
+++ model/userSchema.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePicture: String,
+    batch: {
+        type: String,
+        length: 3,
+        required: true
+    },
+    passingYear: {
+        type: Number,
+        length: 4,
+        required: true
+    },
+    isAlumni: {
+        type: Boolean,
+        default: false
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    reports: Array,
+    chats: Array,
+    posts: Array,
+    questions: Array
+
+})
+
+const User = mongoose.model('Users', userSchema);
+
+module.exports = User;
