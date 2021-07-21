3ee44fe2a337a71f47837619de98a94937c297ad
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-21 22:12:20 +0530

    added signin and verify routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { register } = require("../controllers/auth");
+const { register, verifyUser, signIn } = require("../controllers/auth");
+const { userIdParam } = require("../middlewares/user_middleware");
+
+router.param("userToken", userIdParam);
 
 router.post("/register", register);
+router.post("/signin", signIn);
+router.get("/verify/:userToken", verifyUser);
 
 module.exports = router;
