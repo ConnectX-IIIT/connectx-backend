b28d543be8cb9b7caf721508afd82aa594f5ab94
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-22 11:52:30 +0530

    updated mail templates

--- index.js
+++ index.js
@@ -7,25 +7,15 @@ require('./configs/database');
 const app = express();
 const port = process.env.PORT;
 app.use(express.static('assets'));
-const path = require('path');
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    // res.status(200).json({
-    //     message: 'server running perfectly'
-    // })
-    let options = {
-        root: path.join(__dirname),
-    };
-
-    let fileName = "user_verification_mail.html";
-
-    return res.status(200).sendFile(fileName, options);
+    res.status(200).json({
+        message: 'server running perfectly'
+    })
 })
-
-
 
 app.use('/auth', authRoutes);
 
