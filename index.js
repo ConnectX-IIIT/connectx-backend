6116ba9a0241593491b267dfba578a9673a222b5
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-22 11:04:04 +0530

    updated verification mail template

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ require('./configs/database');
 
 const app = express();
 const port = process.env.PORT;
+app.use(express.static('assets'));
+const path = require('path');
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'server running perfectly'
-    })
+    // res.status(200).json({
+    //     message: 'server running perfectly'
+    // })
+    let options = {
+        root: path.join(__dirname),
+    };
+
+    let fileName = "user_verification_mail.html";
+
+    return res.status(200).sendFile(fileName, options);
 })
+
+
 
 app.use('/auth', authRoutes);
 
