94fe32783d9d1c386ce385f0c0f5818967292d37
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-23 18:19:23 +0530

    separated route signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const { googleLogin } = require("../controllers/auth/google_login");
+const { register } = require("../controllers/auth/register");
 const { signIn } = require("../controllers/auth/signin");
-const { register } = require("../controllers/auth/signup");
+const { signUp } = require("../controllers/auth/signup");
 const { verifyUser } = require("../controllers/auth/verify_user");
 const router = express.Router();
 const { userIdParam } = require("../middlewares/user_middleware");
 
 router.param("userToken", userIdParam);
 
-router.post("/register", register);
+router.post("/signup", signUp);
 router.post("/signin", signIn);
+router.post("/register", register);
 router.post("/googlelogin", googleLogin);
 router.get("/verify/:userToken", verifyUser);
 
