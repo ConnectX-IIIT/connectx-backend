faafa18592329354cbf23d7336f6f8ec3acb9406
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-23 18:17:26 +0530

    made function for add details of user

---
+++ controllers/auth/register.js
@@ -0,0 +1,35 @@
+const User = require('../../model/user_schema');
+require('dotenv').config();
+
+exports.register = async (req, res) => {
+    const { mobile, batch, joiningYear, gender, passingYear, description, userId } = req.body;
+
+    if (!mobile || !batch || !joiningYear || !passingYear || !gender || !description) {
+        return res.status(400).json({
+            error: 'Please fill all details properly'
+        })
+    }
+
+    try {
+        await User.updateOne(
+            { _id: userId }, {
+            $set: {
+                mobile,
+                batch,
+                joiningYear,
+                passingYear,
+                gender,
+                description
+            }
+        });
+
+        return res.status(200).json({
+            message: 'details updated successfully'
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            error: `${err}`
+        })
+    }
+}
