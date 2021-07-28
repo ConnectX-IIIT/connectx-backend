3d013c426a6b076892c4e1a85a2aa6ba3cbd0de9
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-28 23:27:48 +0530

    added images route

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const multer = require('multer');
 const { getDetails } = require("../controllers/user/get_details");
+const { getImage } = require("../controllers/user/get_image");
 const { uploadBackgroundImage } = require("../controllers/user/upload_background_picture");
 const { uploadProfilePicture } = require("../controllers/user/upload_profile_picture");
 const { verifyToken } = require("../middlewares/auth_middleware");
-const { uploadBackground } = require("../middlewares/upload_user_background_picture");
-const { uploadProfile } = require("../middlewares/upload_user_profile_picture");
 
+const upload = multer({ dest: 'assets' });
 const router = express.Router();
 
 router.get('/getdetails', verifyToken, getDetails);
-router.route('/uploadprofile').post(verifyToken, uploadProfile.single('photo'), uploadProfilePicture);
-router.route('/uploadbackground').post(verifyToken, uploadBackground.single('coverPhoto'), uploadBackgroundImage);
+router.get('/images/:key', getImage);
+router.route('/uploadprofile').post(verifyToken, upload.single('photo'), uploadProfilePicture);
+router.route('/uploadbackground').post(verifyToken, upload.single('coverPhoto'), uploadBackgroundImage);
 
 module.exports = router;
