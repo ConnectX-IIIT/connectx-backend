c730490e29525c6a53530710db91a6bb04c40be1
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-03 15:59:06 +0530

    added posts route in home api

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { getPost } = require("../controllers/home/get_post");
 const { search } = require("../controllers/home/search");
 const { verifyToken } = require("../middlewares/auth_middleware");
 const router = express.Router();
 
 router.get("/search/:key", verifyToken, search);
+router.get("/posts", verifyToken, getPost);
 
 module.exports = router;
