b8dab862fd623d34b5cab574d291e1d8190212d8
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-07 19:04:43 +0530

    added addPost route

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { addPost } = require("../controllers/home/add_post");
 const { getPost } = require("../controllers/home/get_post");
 const { search } = require("../controllers/home/search");
 const { verifyToken } = require("../middlewares/auth_middleware");
@@ -6,5 +7,6 @@ const router = express.Router();
 
 router.get("/search/:key", verifyToken, search);
 router.get("/posts", verifyToken, getPost);
+router.get("/addpost", verifyToken, addPost);
 
 module.exports = router;
