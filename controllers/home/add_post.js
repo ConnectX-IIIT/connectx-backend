0182758adcf48bdac84ff6bd91a2332331d3ab0c
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-07 22:12:52 +0530

    used multer to upload multiple files

--- controllers/home/add_post.js
+++ controllers/home/add_post.js
@@ -2,13 +2,13 @@ const Post = require("../../model/post_schema");
 
 exports.addPost = (req, res) => {
     const userId = req.userId;
-    const title = req.body.postTitle;
-    const description = req.body.postDescription;
+    const title = req.body.title;
+    const description = req.body.description;
     const isProject = req.body.isProject;
     const jobLink = req.body.jobLink;
-    const attachedImages = req.file;
-    const imageHeights = req.body.imageHeights;
-    const imageWidths = req.body.imageWidths;
+    const attachedImages = req.files;
+    // const imageHeights = req.body.imageHeights;
+    // const imageWidths = req.body.imageWidths;
 
     // if ( photoWidth > 1500) {
     //     photoHeight = photoHeight * 400 / photoWidth;
@@ -18,5 +18,6 @@ exports.addPost = (req, res) => {
     // const post = new Post({ user: userId, title, description, isProject, jobLink, imageHeights, imageWidths });
     // await post.save();
 
+    // console.log(req.body);
     console.log(attachedImages);
 }
--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const multer = require('multer');
 const { addPost } = require("../controllers/home/add_post");
 const { getPost } = require("../controllers/home/get_post");
 const { search } = require("../controllers/home/search");
 const { verifyToken } = require("../middlewares/auth_middleware");
+
+const upload = multer({ dest: 'assets' });
 const router = express.Router();
 
 router.get("/search/:key", verifyToken, search);
 router.get("/posts", verifyToken, getPost);
-router.post("/addpost", verifyToken, addPost);
+router.route('/addpost').post(verifyToken, upload.array('attachedImgs'), addPost);
 
 module.exports = router;
