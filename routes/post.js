627c28a130db0b7c8ca63019bc8da8a4cd9e886c
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-10 23:10:15 +0530

    added adddiscussion and getdiscussions route in post

--- routes/post.js
+++ routes/post.js
@@ -3,11 +3,15 @@ const { removePost } = require("../controllers/post/remove_post");
 const { vote } = require("../controllers/post/vote");
 const { getPosts } = require("../controllers/post/get_posts");
 const { verifyToken } = require("../middlewares/auth_middleware");
+const { addDiscussion } = require("../controllers/post/add_discussion");
+const { getDiscussions } = require("../controllers/post/get_discussions");
 
 const router = express.Router();
 
 router.post("/vote/:key", verifyToken, vote);
 router.get("/remove/:key", verifyToken, removePost);
 router.get("/getposts", verifyToken, getPosts);
+router.post("/adddiscussion", verifyToken, addDiscussion);
+router.get("/getdiscussions", verifyToken, getDiscussions);
 
 module.exports = router;
