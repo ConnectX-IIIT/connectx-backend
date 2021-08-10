49a56413497eea611ad85657b3d607e2fe0dea1d
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-10 16:09:20 +0530

    added remove route in post.js route file

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const { removePost } = require("../controllers/post/remove_post");
 const { vote } = require("../controllers/post/vote");
 const { verifyToken } = require("../middlewares/auth_middleware");
 
 const router = express.Router();
 
 router.post("/vote/:key", verifyToken, vote);
+router.get("/remove/:key", verifyToken, removePost);
 
 module.exports = router;
