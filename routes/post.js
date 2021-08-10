aacfe60f2e3ed7d1e2e5c84fcc12a79c8a0bbfd8
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-10 19:15:28 +0530

    added getposts route in post

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { removePost } = require("../controllers/post/remove_post");
 const { vote } = require("../controllers/post/vote");
+const { getPosts } = require("../controllers/post/get_posts");
 const { verifyToken } = require("../middlewares/auth_middleware");
 
 const router = express.Router();
 
 router.post("/vote/:key", verifyToken, vote);
 router.get("/remove/:key", verifyToken, removePost);
+router.get("/getposts", verifyToken, getPosts);
 
 module.exports = router;
