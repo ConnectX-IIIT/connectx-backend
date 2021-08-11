12f5fa0e82740a704e7d991831616a66d6cb0bfe
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-11 16:42:12 +0530

    returned replies along with discussions

--- controllers/post/get_discussions.js
+++ controllers/post/get_discussions.js
@@ -3,6 +3,11 @@ const Discussion = require("../../model/discussion_schema");
 exports.getDiscussions = async (req, res) => {
     const discussionIds = req.body.discussionIds;
 
+    let discussions = [{
+        discussion: {},
+        reply: []
+    }];
+
     if (discussionIds.length === 0) {
         return res.status(200).json({
             discussions: []
@@ -10,7 +15,21 @@ exports.getDiscussions = async (req, res) => {
     }
 
     try {
-        const discussions = await Discussion.find({ _id: { $in: discussionIds } }).sort({ "timestamp": -1 });
+        const discussionData = await Discussion.find({ _id: { $in: discussionIds } }).sort({ "timestamp": -1 });
+
+        for (let i = 0; i < discussionData.length; i = i + 1) {
+
+            let replyData = await Discussion.find({ _id: { $in: discussionData[i].replies } }).sort({ "timestamp": -1 });
+            discussions[i].discussion = discussionData[i];
+            discussions[i].reply = replyData;
+
+            if (i !== discussionData.length - 1) {
+                discussions[i + 1] = {
+                    discussion: {},
+                    reply: []
+                }
+            }
+        }
 
         return res.status(200).json({
             discussions
