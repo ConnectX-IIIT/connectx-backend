aaddd3e65e10a7cf1e61b941a8cc0c59386bb004
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-15 14:07:57 +0530

    created addQuestion function to add question

---
+++ controllers/home/add_question.js
@@ -0,0 +1,30 @@
+const User = require("../../model/user_schema");
+const Question = require("../../model/question_schema");
+
+exports.addQuestion = async (req, res) => {
+    const userId = req.userId;
+    const question = req.body.question;
+    const userName = req.body.userName;
+    const userProfile = req.body.userProfile;
+
+    try {
+        const questionInstance = new Question({ user: userId, question, upvotes: 0, userName, userProfile, timestamp: Date.now() });
+        await questionInstance.save();
+
+        await User.updateOne(
+            { _id: userId }, {
+            $push: {
+                questions: questionInstance._id.toString(),
+            }
+        });
+
+        return res.status(200).json({
+            message: `Question added successfully!`,
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            error: `Server error occured!`,
+        });
+    }
+}
