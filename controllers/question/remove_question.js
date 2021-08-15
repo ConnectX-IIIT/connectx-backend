04a2b924b69241a6f110e77748786c29ac2ed9e0
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-15 18:16:11 +0530

    added remove question function

---
+++ controllers/question/remove_question.js
@@ -0,0 +1,50 @@
+const Question = require("../../model/question_schema");
+const User = require("../../model/user_schema");
+
+exports.removeQuestion = async (req, res) => {
+
+    const questionId = req.params.key;
+    const userId = req.userId;
+
+    try {
+        const question = await Question.findOne({ _id: questionId });
+
+        const response = await User.updateOne(
+            { _id: userId },
+            {
+                $pull: {
+                    questions: questionId,
+                },
+            }
+        );
+
+        if (response.nModified === 0) {
+            return res.status(400).json({
+                error: `You can't remove this question!`,
+            });
+        }
+
+        await User.updateMany(
+            { _id: { $in: question.upvotedUsers } },
+            {
+                $pull: {
+                    upvotedQuestions: questionId,
+                },
+            }
+        );
+
+        await User.updateMany(
+            { _id: { $in: question.downvotedUsers } },
+            {
+                $pull: {
+                    downvotedQuestions: questionId,
+                },
+            }
+        );
+
+    } catch (error) {
+        return res.status(500).json({
+            error: `Server error occcured!`,
+        });
+    }
+}
