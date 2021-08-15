dba0260162f77c13cf5dcf42fcc8917fb19d81ea
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-15 14:07:17 +0530

    added addquestion route in home route

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require('multer');
 const { addPost } = require("../controllers/home/add_post");
+const { addQuestion } = require("../controllers/home/add_question");
 const { search } = require("../controllers/home/search");
 const { verifyToken } = require("../middlewares/auth_middleware");
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.get("/search/:key", verifyToken, search);
 router.route('/addpost').post(verifyToken, upload.array('attachedImgs'), addPost);
+router.post('/addquestion', verifyToken, addQuestion);
 
 module.exports = router;
