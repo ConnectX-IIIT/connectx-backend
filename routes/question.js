5a464b992e1073e49ff10473dd3c3b31f9ab850f
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-15 18:16:56 +0530

    added vote and remove routes in question route

--- routes/question.js
+++ routes/question.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { verifyToken } = require("../middlewares/auth_middleware");
 const { getQuestions } = require("../controllers/question/get_questions");
+const { vote } = require("../controllers/question/vote");
+const { removeQuestion } = require("../controllers/question/remove_question");
 
 const router = express.Router();
 
 router.get("/getquestions/:key", verifyToken, getQuestions);
+router.get("/remove/:key", verifyToken, removeQuestion);
+router.post("/vote/:key", verifyToken, vote);
 
 module.exports = router;
