80fba757c718cc623e7f8cae694f33bee0d88e38
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-19 17:56:09 +0530

    removed fetch route to fetch image

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const multer = require('multer');
 const { getDetails } = require("../controllers/user/get_details");
-const { getImage } = require("../controllers/user/get_image");
 const { removeImage } = require("../controllers/user/remove_image");
 const { uploadPicture } = require("../controllers/user/upload_picture");
 const { verifyToken } = require("../middlewares/auth_middleware");
 
-const upload = multer({ dest: 'assets' });
+const storage = multer.diskStorage({});
+const upload = multer({ storage });
 const router = express.Router();
 
 router.get('/getdetails', verifyToken, getDetails);
-router.get('/fetch/:key', getImage);
-router.post('/remove/:key', verifyToken, removeImage);
+router.post('/remove', verifyToken, removeImage);
 router.route('/upload').post(verifyToken, upload.single('photo'), uploadPicture);
 
 module.exports = router;
