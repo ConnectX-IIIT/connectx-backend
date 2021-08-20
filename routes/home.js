6e4a6cd7e8b382e90ddbf77014c0e905b53d1c43
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-20 21:30:04 +0530

    added fetchusers route in home route

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require('multer');
 const { addPost } = require("../controllers/home/add_post");
 const { addQuestion } = require("../controllers/home/add_question");
+const { fetchUsers } = require("../controllers/home/fetch_users");
 const { search } = require("../controllers/home/search");
 const { verifyToken } = require("../middlewares/auth_middleware");
 
@@ -12,5 +13,6 @@ const router = express.Router();
 router.get("/search/:key", verifyToken, search);
 router.route('/addpost').post(verifyToken, upload.array('attachedImgs'), addPost);
 router.post('/addquestion', verifyToken, addQuestion);
+router.get('/fetchusers', verifyToken, fetchUsers);
 
 module.exports = router;
