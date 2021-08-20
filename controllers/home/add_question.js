42fb22b2d81555096389117dfe45b4824f6ad97b
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2021-08-20 11:27:11 +0530

    Revert "removed userProfile and userName in question"
    This reverts commit 2977eac8f1af1438905573a8727762e7b4f93f03.

--- controllers/home/add_question.js
+++ controllers/home/add_question.js
@@ -4,6 +4,8 @@ const Question = require("../../model/question_schema");
 exports.addQuestion = async (req, res) => {
     const userId = req.userId;
     const question = req.body.question;
+    const userName = req.body.userName;
+    const userProfile = req.body.userProfile;
 
     if (!question) {
         return res.status(400).json({
@@ -12,7 +14,7 @@ exports.addQuestion = async (req, res) => {
     }
 
     try {
-        const questionInstance = new Question({ user: userId, question, upvotes: 0, timestamp: Date.now() });
+        const questionInstance = new Question({ user: userId, question, upvotes: 0, userName, userProfile, timestamp: Date.now() });
         await questionInstance.save();
 
         await User.updateOne(
