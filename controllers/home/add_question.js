2977eac8f1af1438905573a8727762e7b4f93f03
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-20 11:05:16 +0530

    removed userProfile and userName in question

--- controllers/home/add_question.js
+++ controllers/home/add_question.js
@@ -4,8 +4,6 @@ const Question = require("../../model/question_schema");
 exports.addQuestion = async (req, res) => {
     const userId = req.userId;
     const question = req.body.question;
-    const userName = req.body.userName;
-    const userProfile = req.body.userProfile;
 
     if (!question) {
         return res.status(400).json({
@@ -14,7 +12,7 @@ exports.addQuestion = async (req, res) => {
     }
 
     try {
-        const questionInstance = new Question({ user: userId, question, upvotes: 0, userName, userProfile, timestamp: Date.now() });
+        const questionInstance = new Question({ user: userId, question, upvotes: 0, timestamp: Date.now() });
         await questionInstance.save();
 
         await User.updateOne(
