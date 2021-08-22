adbb70c82b2470dcf8c8df83342667b2d9f0ed3a
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-22 16:24:23 +0530

    renamed register file in controller

---
+++ controllers/auth/add_extra_details.js
@@ -0,0 +1,37 @@
+const User = require('../../model/user_schema');
+require('dotenv').config();
+
+exports.addExtraDetails = async (req, res) => {
+    const { mobile, batch, joiningYear, gender, passingYear, description, isAlumni, userId } = req.body;
+
+    if (!mobile || !batch || !joiningYear || !passingYear || !gender || !description) {
+        return res.status(400).json({
+            error: 'Please fill all details properly!'
+        })
+    }
+
+    try {
+        await User.updateOne(
+            { _id: userId }, {
+            $set: {
+                mobile,
+                batch,
+                joiningYear,
+                passingYear,
+                gender,
+                isAlumni,
+                description,
+                isVerified: false
+            }
+        });
+
+        return res.status(200).json({
+            message: 'details updated successfully'
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            error: `Server error occured!`
+        })
+    }
+}
