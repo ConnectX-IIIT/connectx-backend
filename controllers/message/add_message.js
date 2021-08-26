af9f1282dd5e036a2ba9d51cd9dbbec35629eeb7
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-26 10:56:57 +0530

    updated lastMessage and time on new message in chat

--- controllers/message/add_message.js
+++ controllers/message/add_message.js
@@ -1,3 +1,5 @@
+const Conversation = require("../../model/conversation_schema");
+const Group = require("../../model/group_schema");
 const Message = require("../../model/message_schema");
 
 exports.addMessage = async (req, res) => {
@@ -6,6 +8,7 @@ exports.addMessage = async (req, res) => {
     const userName = req.body.name;
     const conversationId = req.params.key;
     const reference = req.body.reference;
+    const isGroup = req.body.isGroup;
 
     if (!message) {
         return res.status(400).json({
@@ -16,6 +19,24 @@ exports.addMessage = async (req, res) => {
     try {
         const messageInstance = new Message({ userId, message, userName, reference, conversationId });
         await messageInstance.save();
+
+        if (isGroup) {
+            await Group.updateOne(
+                { _id: conversationId }, {
+                $set: {
+                    lastMessage: message,
+                    lastModified: Date.now(),
+                }
+            });
+        } else {
+            await Conversation.updateOne(
+                { _id: conversationId }, {
+                $set: {
+                    lastMessage: message,
+                    lastModified: Date.now(),
+                }
+            });
+        }
 
         return res.status(200).json({
             message: messageInstance,
