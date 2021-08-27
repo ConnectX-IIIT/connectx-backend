12a00a30bffba0413403dfdefe30790b7a00edf9
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-27 20:55:49 +0530

    added middleware in question route to check if user is verified

--- routes/question.js
+++ routes/question.js
@@ -3,11 +3,12 @@ const { verifyToken } = require("../middlewares/auth_middleware");
 const { getQuestions } = require("../controllers/question/get_questions");
 const { vote } = require("../controllers/question/vote");
 const { removeQuestion } = require("../controllers/question/remove_question");
+const { isUserVerified } = require("../middlewares/user_verified");
 
 const router = express.Router();
 
 router.get("/getquestions/:key", verifyToken, getQuestions);
-router.get("/remove/:key", verifyToken, removeQuestion);
-router.post("/vote/:key", verifyToken, vote);
+router.get("/remove/:key", verifyToken, isUserVerified, removeQuestion);
+router.post("/vote/:key", verifyToken, isUserVerified, vote);
 
 module.exports = router;
