6042c4c805d51704f5327da54a54f1ecbabd9937
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-27 20:55:14 +0530

    added middleware in home route to check if user is verified

--- routes/home.js
+++ routes/home.js
@@ -5,14 +5,15 @@ const { addQuestion } = require("../controllers/home/add_question");
 const { fetchUsers } = require("../controllers/home/fetch_users");
 const { search } = require("../controllers/home/search");
 const { verifyToken } = require("../middlewares/auth_middleware");
+const { isUserVerified } = require("../middlewares/user_verified");
 
 const storage = multer.diskStorage({});
 const upload = multer({ storage });
 const router = express.Router();
 
 router.get("/search/:key", verifyToken, search);
-router.route('/addpost').post(verifyToken, upload.array('attachedImgs'), addPost);
-router.post('/addquestion', verifyToken, addQuestion);
+router.route('/addpost').post(verifyToken, isUserVerified, upload.array('attachedImgs'), addPost);
+router.post('/addquestion', verifyToken, isUserVerified, addQuestion);
 router.get('/fetchusers', verifyToken, fetchUsers);
 
 module.exports = router;
