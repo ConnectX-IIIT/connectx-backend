d95daf82d4a53cedd97ac061e330c17c472c944f
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-27 20:54:44 +0530

    added middleware in conversation route to check if user is verified

--- routes/conversation.js
+++ routes/conversation.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { verifyToken } = require("../middlewares/auth_middleware");
 const { addConversation } = require("../controllers/conversation/add_conversation");
 const { getConversations } = require("../controllers/conversation/get_conversations");
+const { isUserVerified } = require("../middlewares/user_verified");
 
 const router = express.Router();
 
 router.post("/getconversations", verifyToken, getConversations);
-router.post("/addconversation", verifyToken, addConversation);
+router.post("/addconversation", verifyToken, isUserVerified, addConversation);
 
 module.exports = router;
