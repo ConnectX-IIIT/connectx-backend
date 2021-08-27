c1b77fe0a6e722344d45df914a0f67d56290bf70
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-27 20:55:36 +0530

    added middleware in post route to check if user is verified

--- routes/post.js
+++ routes/post.js
@@ -4,12 +4,13 @@ const { vote } = require("../controllers/post/vote");
 const { getPosts } = require("../controllers/post/get_posts");
 const { verifyToken } = require("../middlewares/auth_middleware");
 const { addDiscussion } = require("../controllers/post/add_discussion");
+const { isUserVerified } = require("../middlewares/user_verified");
 
 const router = express.Router();
 
-router.post("/vote/:key", verifyToken, vote);
-router.get("/remove/:key", verifyToken, removePost);
+router.post("/vote/:key", verifyToken, isUserVerified, vote);
+router.get("/remove/:key", verifyToken, isUserVerified, removePost);
 router.get("/getposts/:key", verifyToken, getPosts);
-router.post("/adddiscussion", verifyToken, addDiscussion);
+router.post("/adddiscussion", verifyToken, isUserVerified, addDiscussion);
 
 module.exports = router;
