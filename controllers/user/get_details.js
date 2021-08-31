168dc4cb30fe34b6af70e9decc8a53950f99cd34
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-31 16:27:33 +0530

    added route to fetch other users details

--- controllers/user/get_details.js
+++ controllers/user/get_details.js
@@ -3,9 +3,15 @@ const User = require('../../model/user_schema');
 exports.getDetails = async (req, res) => {
 
     const userId = req.userId;
+    const otherUserId = req.params.userId;
+    let userDetails;
 
     try {
-        const userDetails = await User.findOne({ _id: userId });
+        if (otherUserId) {
+            userDetails = await User.findOne({ _id: otherUserId });
+        } else {
+            userDetails = await User.findOne({ _id: userId });
+        }
 
         return res.status(200).json({
             userData: userDetails
--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,14 @@ const { removeImage } = require("../controllers/user/remove_image");
 const { updateDetails } = require("../controllers/user/update_details");
 const { uploadPicture } = require("../controllers/user/upload_picture");
 const { verifyToken } = require("../middlewares/auth_middleware");
+const { isUserVerified } = require("../middlewares/user_verified");
 
 const storage = multer.diskStorage({});
 const upload = multer({ storage });
 const router = express.Router();
 
 router.get('/getdetails', verifyToken, getDetails);
+router.get('/getdetails/:userId', verifyToken, isUserVerified, getDetails);
 router.post('/removephoto', verifyToken, removeImage);
 router.post('/updatedetails', verifyToken, updateDetails);
 router.route('/upload').post(verifyToken, upload.single('photo'), uploadPicture);
