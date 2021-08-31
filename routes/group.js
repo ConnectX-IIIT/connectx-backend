b7690aaf7eb356deadff59017c7d4d15891e968f
HarshilMendpara <mendparaharshil@gmail.com>
2021-08-31 14:50:16 +0530

    used multer to upload photo in cloudinary

--- routes/group.js
+++ routes/group.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const multer = require('multer');
 const { updateDescription } = require("../controllers/group/update_description");
 const { updateGroupProfilePicture } = require("../controllers/group/update_profile_picture");
 const { verifyToken } = require("../middlewares/auth_middleware");
 const { isUserVerified } = require("../middlewares/user_verified");
 
+const storage = multer.diskStorage({});
+const upload = multer({ storage });
 const router = express.Router();
 
-router.post("/updateprofile", verifyToken, isUserVerified, updateGroupProfilePicture);
+router.post("/updateprofile", verifyToken, isUserVerified, upload.single('photo'), updateGroupProfilePicture);
 router.post("/updatedescription", verifyToken, isUserVerified, updateDescription);
 
 module.exports = router;
