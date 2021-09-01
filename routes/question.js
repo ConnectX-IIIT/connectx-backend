928cbd3a5855c9189299255df304df5d60358433
HarshilMendpara <mendparaharshil@gmail.com>
2021-09-01 16:50:46 +0530

    added addanswer route in question route

--- routes/question.js
+++ routes/question.js
@@ -4,9 +4,11 @@ const { getQuestions } = require("../controllers/question/get_questions");
 const { vote } = require("../controllers/question/vote");
 const { removeQuestion } = require("../controllers/question/remove_question");
 const { isUserVerified } = require("../middlewares/user_verified");
+const { addAnswer } = require("../controllers/question/add_answer");
 
 const router = express.Router();
 
+router.post("/addanswer/:questionId", verifyToken, isUserVerified, addAnswer);
 router.get("/getquestions/:key", verifyToken, getQuestions);
 router.get("/remove/:key", verifyToken, isUserVerified, removeQuestion);
 router.post("/vote/:key", verifyToken, isUserVerified, vote);
