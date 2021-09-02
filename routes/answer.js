0549f9474510cd0a976bf5b7931b9dd5a46b3e0a
HarshilMendpara <mendparaharshil@gmail.com>
2021-09-02 15:40:33 +0530

    added addcomment and remove answer routes

--- routes/answer.js
+++ routes/answer.js
@@ -3,10 +3,14 @@ const { verifyToken } = require("../middlewares/auth_middleware");
 const { vote } = require("../controllers/answer/vote");
 const { isUserVerified } = require("../middlewares/user_verified");
 const { getAnswers } = require("../controllers/answer/get_answers");
+const { addComment } = require("../controllers/answer/add_comment");
+const { removeAnswer } = require("../controllers/answer/remove_answer");
 
 const router = express.Router();
 
 router.post("/getanswers", verifyToken, getAnswers);
+router.post("/addcomment", verifyToken, isUserVerified, addComment);
+router.get("/remove/:key", verifyToken, isUserVerified, removeAnswer);
 router.post("/vote/:key", verifyToken, isUserVerified, vote);
 
 module.exports = router;
