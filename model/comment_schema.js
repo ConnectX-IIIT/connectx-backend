052922da1949dee21f26472470be54fcdbf66c20
HarshilMendpara <mendparaharshil@gmail.com>
2021-09-02 15:17:42 +0530

    created comment schema

---
+++ model/comment_schema.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    answerId: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    userProfile: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Number,
+        required: true
+    },
+    reactions: {
+        type: Number
+    },
+    reports: {
+        type: Array
+    },
+    replies: {
+        type: Array
+    },
+    upvotedUsers: {
+        type: Array
+    },
+    downvotedUsers: {
+        type: Array
+    },
+    reference: {
+        type: String
+    }
+})
+
+const Comment = mongoose.model('comments', commentSchema);
+
+module.exports = Comment;
