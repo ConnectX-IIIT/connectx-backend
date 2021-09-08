f824bfc2ff13a14346946e52699c9074c2b08cb9
HarshilMendpara <mendparaharshil@gmail.com>
2021-09-08 16:48:45 +0530

    added updatepost route in post route

--- routes/post.js
+++ routes/post.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const multer = require('multer');
 const { removePost } = require("../controllers/post/remove_post");
 const { vote } = require("../controllers/post/vote");
 const { getPosts } = require("../controllers/post/get_posts");
 const { verifyToken } = require("../middlewares/auth_middleware");
 const { addDiscussion } = require("../controllers/post/add_discussion");
 const { isUserVerified } = require("../middlewares/user_verified");
+const { updatePost } = require("../controllers/post/update_post");
 
+const storage = multer.diskStorage({});
+const upload = multer({ storage });
 const router = express.Router();
 
 router.post("/vote/:key", verifyToken, isUserVerified, vote);
+router.route('/updatepost').post(verifyToken, isUserVerified, upload.array('attachedImgs'), updatePost);
 router.get("/remove/:key", verifyToken, isUserVerified, removePost);
 router.get("/getposts/:key", verifyToken, getPosts);
 router.post("/adddiscussion", verifyToken, isUserVerified, addDiscussion);
